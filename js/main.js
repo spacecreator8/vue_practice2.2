f418d6a96b039f961368c546bda678147f7417b4
spacecreator8 <spcc888@ya.ru>
2024-05-02 15:58:09 +0700

    Сделал обработчик создания объекта и передачей его в шину, начал работу с колонками

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ Vue.component('column', {
         column_name:{
             type: String,
             required: true,
+        },
+        column_id:{
+            type: String,
+            required: true,
+        }
+        
+    },
+    data(){
+        return{
+            listsArray: [],
         }
     },
     template:`
@@ -20,11 +30,11 @@ Vue.component('creator', {
     template:`
         <form>
             <p><b>Заголовок:</b> <input type="text" v-model="blank.title"></p>
-            <p>Задача - 1: <input type="text" v-model="blank.tasks[0].name"></p>
-            <p>Задача - 2: <input type="text" v-model="blank.tasks[1].name"></p>
-            <p>Задача - 3: <input type="text" v-model="blank.tasks[2].name"></p>
-            <p v-if="!hiddenFlag4">Задача - 4: <input type="text" v-model="blank.tasks[3].name"></p>
-            <p v-if="!hiddenFlag5">Задача - 5: <input type="text" v-model="blank.tasks[4].name"></p>
+            <p>Задача - 1: <input type="text" v-model="blank.tasks.task1.name"></p>
+            <p>Задача - 2: <input type="text" v-model="blank.tasks.task2.name"></p>
+            <p>Задача - 3: <input type="text" v-model="blank.tasks.task3.name"></p>
+            <p v-if="!hiddenFlag4">Задача - 4: <input type="text" v-model="blank.tasks.task4.name"></p>
+            <p v-if="!hiddenFlag5">Задача - 5: <input type="text" v-model="blank.tasks.task5.name"></p>
             <button v-if="hiddenFlag5" @click.prevent="addTask">+++</button>
             <button  @click.prevent="customSubmit">Добавить</button>
         </form>
@@ -69,9 +79,39 @@ Vue.component('creator', {
                 this.hiddenFlag5 = false;
             }
         },
-        customSubmit(){//Буду делать через object.assign - 
+        customSubmit(){//Проверил, копирование адекватное, после копирования обнулил болванку, вывел копию в консоль - данные сохранились в копии после сброса болванки.
             let copy = Object.assign({}, this.blank);
-            copy
+            
+            copy.tasks = Object.assign({}, this.blank.tasks);
+            for(let i in this.blank.tasks){
+                copy.tasks[i] = Object.assign({}, this.blank.tasks[i]);
+            }
+            this.blank = {
+                title: null,
+                tasks:{
+                    task1: {
+                        name: null,
+                        activity: false
+                    },
+                    task2: {
+                        name: null,
+                        activity: false
+                    },
+                    task3: {
+                        name: null,
+                        activity: false
+                    },
+                    task4: {
+                        name: null,
+                        activity: false
+                    },
+                    task5: {
+                        name: null,
+                        activity: false
+                    },
+                }
+            }
+            eventBus.$emit('takeFromForm', copy);
         }
 
     }
